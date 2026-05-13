390a8b7f33967b363a8edc33b1a455bfd17a2883
bernardoJaco <bernardoesjaco@gmail.com>
2026-05-13 19:12:28 +0100

    service definitions.

--- code/jvm/STC-SM-Backend/service/src/main/kotlin/ActivityService.kt
+++ code/jvm/STC-SM-Backend/service/src/main/kotlin/ActivityService.kt
@@ -1,12 +1,29 @@
 import errors.ActivityError
 import org.springframework.stereotype.Component
 import activity.Activity
+import activity.ActivityType
 import utils.Either
+import java.time.Instant
 
 @Component
 class ActivityService {
-
     fun getActivity(id: Int): Either<ActivityError, Activity> {
         TODO("Implement activity fetching logic")
+    }
+
+    fun getActivityByTool(tid: Int): Either<ActivityError, Activity> {
+        TODO()
+    }
+
+    fun getActivityByUser(uid: Int): Either<ActivityError, Activity> {
+        TODO()
+    }
+
+    fun getActivityByCabinet(cid: Int): Either<ActivityError, Activity> {
+        TODO()
+    }
+
+    fun createActivity(uid: Int, type: ActivityType, date: Instant): Either<ActivityError, Activity> {
+        TODO()
     }
 }
--- code/jvm/STC-SM-Backend/service/src/main/kotlin/ShiftService.kt
+++ code/jvm/STC-SM-Backend/service/src/main/kotlin/ShiftService.kt
@@ -2,11 +2,19 @@ import errors.ShiftError
 import org.springframework.stereotype.Component
 import shift.Shift
 import utils.Either
+import java.time.Instant
 
 @Component
 class ShiftService {
-
     fun getShift(id: Int): Either<ShiftError, Shift> {
         TODO("Implement shift fetching logic")
+    }
+
+    fun updateShift(id : Int): Either<ShiftError, Shift> {
+        TODO()
+    }
+
+    fun createShift(uid: Int, cid: Int, startTime: Instant, endTime: Instant): Either<ShiftError, Shift> {
+        TODO()
     }
 }
