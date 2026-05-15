b633312859fc2788a73b425a4298cde902a7378b
bernardoJaco <bernardoesjaco@gmail.com>
2026-05-15 19:37:57 +0100

    create user done.

--- code/jvm/STC-SM-Backend/app/src/main/kotlin/pt/isel/DataInitializer.kt
+++ code/jvm/STC-SM-Backend/app/src/main/kotlin/pt/isel/DataInitializer.kt
@@ -1,16 +1,24 @@
 package pt.isel
 
 import org.springframework.boot.CommandLineRunner
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
+import pt.isel.auth.PasswordValidationInfo
 import pt.isel.profile.Profile
 import pt.isel.profile.Role
+import pt.isel.user.User
+import pt.isel.user.UserRepository
+import pt.isel.user.UserStatus
 
+// Initializes the Database with the 3 Profiles and the Admin User.
 @Component
 class DataInitializer(
-    private val profileRepository: ProfileRepository
+    private val profileRepository: ProfileRepository,
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
 ) : CommandLineRunner {
-    override fun run(vararg args: String?) {
-        // Load initial profiles if they don't exist
+    override fun run(vararg args: String) {
+        // Checks if the table is empty
         if (profileRepository.count() == 0L) {
             val profiles = listOf(
                 Profile(id = 1, role = Role.MECHANIC, description = "Mechanic"),
@@ -18,6 +26,16 @@ class DataInitializer(
                 Profile(id = 3, role = Role.ADMIN, description = "Administrator")
             )
             profileRepository.saveAll(profiles)
+        }
+        if (userRepository.count() == 0L) {
+            val userAdmin = User(
+                name = "Admin",
+                email = "admin@isel.pt",
+                profile = Profile(id = 3, role = Role.ADMIN, description = "Administrator"),
+                status = UserStatus.ACTIVE,
+                passwordValidation = PasswordValidationInfo(passwordEncoder.encode("admin")!!)
+            )
+            userRepository.save(userAdmin)
         }
     }
 }
